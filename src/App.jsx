62df4c59ac91f1ab4113c873c72fbc25dfb968d8
Jon <jonprosser84@gmail.com>
2025-09-27 16:37:49 +0100

    fadded a filter search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { useState, useEffect } from "react";
 import CoinCard from "./components/CoinCard";
+import LimitSelector from "./components/LimitSelector";
+import FilterInput from "./components/FilterInput";
 
-const API_URL =
-  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const App = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [limit, setLimit] = useState(10);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     const fetchCoins = async () => {
       try {
-        const res = await fetch(API_URL);
+        const res = await fetch(
+          `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+        );
         if (!res.ok) throw new Error("Failed to fetch data!");
         const data = await res.json();
         setCoins(data);
@@ -23,7 +28,11 @@ const App = () => {
       }
     };
     fetchCoins();
-  }, []);
+  }, [limit]);
+
+  const filteredCoins = coins.filter((coin) => {
+    return coin.name.toLowerCase().includes(filter.toLowerCase()) ||  coin.symbol.toLowerCase().includes(filter.toLowerCase())
+  })
 
   return (
     <div>
@@ -31,13 +40,16 @@ const App = () => {
       {loading && <p>...Loading</p>}
       {error && <div className="error">{error}</div>}
 
+      <div className="top-controls">
+          <FilterInput filter={filter} onFilterChange={setFilter}/>
+          <LimitSelector limit={limit} onLimitChange={setLimit}/>
+      </div>
+
       {!loading && !error && (
         <main className="grid">
-          {coins.map((coin) => (
-
-            <CoinCard coin={coin}/>
-      
-          ))}
+          {filteredCoins.length > 0 ? filteredCoins.map((coin) => (
+            <CoinCard coin={coin} />
+          )) : <p>No matching coins</p>}
         </main>
       )}
     </div>
